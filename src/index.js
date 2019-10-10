df8cce45bb606a98ac23f19156aaab265c5a6118
omairapalacios <55103932+omairapalacios@users.noreply.github.com>
2019-10-10 01:40:21 -0500

    modificando estilos y agregando vistas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 let offset
 let string
 
-document.getElementById('btn-encode').addEventListener('click', () =>{ // 
+document.getElementById('login-medic').style.display='none'
+document.getElementById('login-patient').style.display='none'
+document.getElementById('diagnostic-medic-patient').style.display='none'
+document.getElementById('btn-go-back').style.display='none'
+
+document.getElementById('btn-medic').addEventListener('click',() =>{ 
+  document.getElementById('login-medic').style.display='block'
+  document.getElementById('btn-go-back').style.display='block'
+  document.getElementById('login').style.display='none'
+})
+document.getElementById('btn-patient').addEventListener('click',() =>{
+  document.getElementById('login-patient').style.display='block'
+  document.getElementById('btn-go-back').style.display='block'
+  document.getElementById('login-medic').style.display='none'
+  document.getElementById('login').style.display='none'
+})
+document.getElementById('btn-send-medic').addEventListener('click',() =>{
+  document.getElementById('diagnostic-medic-patient').style.display='block'
+  document.getElementById('btn-go-back').style.display='block'
+  document.getElementById('login-patient').style.display='none'
+  document.getElementById('login-medic').style.display='none'
+})
+document.getElementById('btn-send-patient').addEventListener('click',() =>{
+  document.getElementById('diagnostic-medic-patient').style.display='block'
+  document.getElementById('btn-go-back').style.display='block'
+  document.getElementById('login-patient').style.display='none'
+  document.getElementById('login-medic').style.display='none'
+})
+document.getElementById('btn-go-back').addEventListener('click',() =>{
+  document.getElementById('login').style.display='block'
+  document.getElementById('diagnostic-medic-patient').style.display='none'
+  document.getElementById('btn-go-back').style.display='none'
+})
+
+document.getElementById('btn-encode').addEventListener('click', () =>{ 
   offset=document.getElementById('offset-Text').value
   string = document.getElementById('string-Text').value
   cipher.encode(offset, string)  
@@ -11,4 +45,6 @@ document.getElementById('btn-decode').addEventListener('click', () =>{
   string = document.getElementById('string-Text2').value
   cipher.decode(offset, string)
 })
+
+
 
