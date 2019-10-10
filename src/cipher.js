b0de7daaa58557766118327bdee1a2279bd322f7
omairapalacios <55103932+omairapalacios@users.noreply.github.com>
2019-10-10 01:46:36 -0500

    agregando validaciones

--- src/cipher.js
+++ src/cipher.js
@@ -5,6 +5,7 @@ encode: (offset, string) => {
 let asciiString
 let result=''
 
+if (offset > 0){
   for (let i = 0; i < string.length; i++) {
     asciiString = string.charCodeAt(i)
     if (asciiString >= 65 && asciiString <= 90) { // conditional to uppercase
@@ -18,24 +19,32 @@ let result=''
     }
     document.getElementById('string-Text2').innerHTML = result 
   } 
+} else {
+    result= alert ("Por favor, ingrese un numero mayor a 0 :)") // to validate offset from input
+}
 }
 ,
 
 decode: (offset, string) => {
   let asciiString
   let result=''
-  for (let i = 0; i < string.length; i++) {
-    asciiString = string.charCodeAt(i)
-    if (asciiString >= 65 && asciiString <= 90) { // conditional to uppercase
-      result += String.fromCharCode (((asciiString - 90 - parseInt(offset)) % 26)+90)
-    } else if (asciiString >= 97 && asciiString <= 122) { // conditional to lowercase
-      result += String.fromCharCode (((asciiString - 122 - parseInt(offset)) % 26) +122)
-    } else if (asciiString >= 48 && asciiString <= 57) { // conditional to numbers
-      result += String.fromCharCode (((asciiString - 57 - parseInt(offset)) % 10 ) + 57)
-    } else {
-      result += String.fromCharCode(asciiString) // conditional to any character
-    }
-    document.getElementById('string-Text2').innerHTML = result   
-  } 
+  if( offset > 0) {
+    for (let i = 0; i < string.length; i++) {
+      asciiString = string.charCodeAt(i)
+      if (asciiString >= 65 && asciiString <= 90) { // conditional to uppercase
+        result += String.fromCharCode (((asciiString - 90 - parseInt(offset)) % 26)+90)
+      } else if (asciiString >= 97 && asciiString <= 122) { // conditional to lowercase
+        result += String.fromCharCode (((asciiString - 122 - parseInt(offset)) % 26) +122)
+      } else if (asciiString >= 48 && asciiString <= 57) { // conditional to numbers
+        result += String.fromCharCode (((asciiString - 57 - parseInt(offset)) % 10 ) + 57)
+      } else {
+        result += String.fromCharCode(asciiString) // conditional to any character
+      }
+      document.getElementById('string-Text2').innerHTML = result   
+      } 
+  } else {
+      result= alert ("Por favor, ingrese un numero mayor a 0 :)") // to validate offset from input
+  }
 }
+
 };
